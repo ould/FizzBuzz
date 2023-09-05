0deb8c7622ca28b55a6ec9ac431edc7ba986e312
DenisMac <denis.hellal@codeworks.fr>
2023-09-05 13:49:41 +0200

    Mise en place du 1er test iter1

---
+++ FizzBuzz/FizzBuzzIterations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FizzBuzz
+{
+	public class FizzBuzzIterations
+    {
+		public FizzBuzzIterations()
+		{
+		}
+
+        public string Iter1(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ TestFizzBuzz/UnitTestFizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿using FizzBuzz;
+namespace TestFizzBuzz;
+
+[TestClass]
+public class UnitTestFizzBuzz
+{
+    [TestMethod]
+    public void TestMethodIter1Donne3RenvoieFizz()
+    {
+        var FizzBuzzObject = new FizzBuzzIterations();
+        string result = FizzBuzzObject.Iter1(3);
+        Assert.Equals(result, "Fizz");
+
+    }
+}

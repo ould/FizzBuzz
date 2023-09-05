8e38e0835f8e89e7311a4c83a58f44bf16658e2f
DenisMac <denis.hellal@codeworks.fr>
2023-09-05 13:58:34 +0200

    Refacto de la classe de test

--- TestFizzBuzz/UnitTestFizzBuzz.cs
+++ TestFizzBuzz/UnitTestFizzBuzz.cs
@@ -4,11 +4,17 @@ namespace TestFizzBuzz;
 [TestClass]
 public class UnitTestFizzBuzz
 {
+    private FizzBuzzIterations _fizzBuzzObject;
+
+    public UnitTestFizzBuzz()
+    {
+        _fizzBuzzObject = new FizzBuzzIterations();
+    }
+
     [TestMethod]
     public void TestMethodIter1Donne3RenvoieFizz()
     {
-        var FizzBuzzObject = new FizzBuzzIterations();
-        string result = FizzBuzzObject.Iter1(3);
+        string result = _fizzBuzzObject.Iter1(3);
         Assert.AreEqual(result, "Fizz");
 
     }

f3ed5fb3d5a881b5dca4663d3a8e2f142f401ff8
DenisMac <denis.hellal@codeworks.fr>
2023-09-05 14:04:56 +0200

    Iter1 : Ajout logique pour divisible par 5 (tests passés)

--- FizzBuzz/FizzBuzzIterations.cs
+++ FizzBuzz/FizzBuzzIterations.cs
@@ -10,6 +10,7 @@ namespace FizzBuzz
         public string Iter1(int valeur)
         {
             if (valeur % 3 == 0) return "Fizz";
+            if (valeur % 5 == 0) return "Buzz";
             return valeur.ToString();
         }
     }

69452c3a75cdeb9f0e4a414be40065f8a304b8d3
DenisMac <denis.hellal@codeworks.fr>
2023-09-05 14:20:16 +0200

    Iter2 : mise en place des tests

--- TestFizzBuzz/UnittestFizzBuzzIter2.cs
+++ TestFizzBuzz/UnittestFizzBuzzIter2.cs
@@ -11,6 +11,58 @@ public class UnittestFizzBuzzIter2
         Assert.AreEqual(result, "Fizz");
 
     }
+
+
+    [TestMethod]
+    public void TestMethodIter2Donne14RenvoieFizz()
+    {
+        string result = FizzBuzzIterations.Iter2(14);
+        Assert.AreEqual(result, "Fizz");
+
+    }
+
+
+    [TestMethod]
+    public void TestMethodIter2Donne11RenvoieBuzz()
+    {
+        string result = FizzBuzzIterations.Iter2(11);
+        Assert.AreEqual(result, "Buzz");
+
+    }
+
+    [TestMethod]
+    public void TestMethodIter2Donne22RenvoieBuzz()
+    {
+        string result = FizzBuzzIterations.Iter2(22);
+        Assert.AreEqual(result, "Buzz");
+
+    }
+
+
+    [TestMethod]
+    public void TestMethodIter2Donne77RenvoieFizzBuzz()
+    {
+        string result = FizzBuzzIterations.Iter2(77);
+        Assert.AreEqual(result, "FizzBuzz");
+
+    }
+
+    [TestMethod]
+    public void TestMethodIter2Donne1Renvoie1()
+    {
+        string result = FizzBuzzIterations.Iter2(1);
+        Assert.AreEqual(result, "1");
+
+    }
+
+    [TestMethod]
+    public void TestMethodIter2Donne3Renvoie3()
+    {
+        string result = FizzBuzzIterations.Iter2(3);
+        Assert.AreEqual(result, "3");
+
+    }
+
 }
 
 

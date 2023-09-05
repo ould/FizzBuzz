cdaa675a4d4db0fe91741910083d7bf2a539ad4c
DenisMac <denis.hellal@codeworks.fr>
2023-09-05 14:35:32 +0200

    Refacto : suite et suppession commentaire

--- FizzBuzz/FizzBuzzIterations.cs
+++ FizzBuzz/FizzBuzzIterations.cs
@@ -8,15 +8,8 @@ namespace FizzBuzz
 
         public static string Iter1(int valeur)
         {
-            //var result = new StringBuilder();
-            //if (valeur % 3 == 0) result.Append("Fizz");
-            //if (valeur % 5 == 0) result.Append("Buzz");
-
-            //return result.Length > 0 ? result.ToString() : valeur.ToString();
-
             var result = FizzBuzzGenerator(valeur, 3, 5);
             return result;
-
         }
 
         public static string Iter2(int valeur)

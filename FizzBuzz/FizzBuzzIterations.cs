5b3734efc4076ef5103a34e9aec8ff95f1c3493d
DenisMac <denis.hellal@codeworks.fr>
2023-09-05 16:28:18 +0200

    SuperFizzBuzz : premiere refacto (test OK)

--- FizzBuzz/FizzBuzzIterations.cs
+++ FizzBuzz/FizzBuzzIterations.cs
@@ -1,66 +1,73 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace FizzBuzz
 {
 	public class FizzBuzzIterations
     {
-        private IDictionary<int, string> _lienInputResult;
+        private IDictionary<Func<int, bool>, string> _lienInputResult;
 
         public FizzBuzzIterations()
         {
-            _lienInputResult = new Dictionary<int, string>();
+            _lienInputResult = new Dictionary<Func<int, bool>, string>();
         }
 
         public string Iter1(int input)
         {
             
-            _lienInputResult.Add(3, "Fizz");
-            _lienInputResult.Add(5, "Buzz");
+            _lienInputResult.Add(i => i%3 == 0, "Fizz");
+            _lienInputResult.Add(i => i%5 == 0, "Buzz");
 
-            return FizzBuzzGameGenerator(input, _lienInputResult);
+            return SuperFizzBuzzGenerator(input, _lienInputResult);
 
         }      
 
         public string Iter2(int input)
         {    
-            _lienInputResult.Add(7, "Fizz");
-            _lienInputResult.Add(11, "Buzz");
+            _lienInputResult.Add(i => i%7 == 0, "Fizz");
+            _lienInputResult.Add(i => i%11 == 0, "Buzz");
 
-            return FizzBuzzGameGenerator(input, _lienInputResult);
+            return SuperFizzBuzzGenerator(input, _lienInputResult);
         }
 
 
         public string Iter3(int input)
         {
-            _lienInputResult.Add(13, "Fizz");
-            _lienInputResult.Add(17, "Buzz");
-            _lienInputResult.Add(19, "Fuzz");
-            _lienInputResult.Add(23, "Bizz");
+            _lienInputResult.Add(i=> i%13 ==0, "Fizz");
+            _lienInputResult.Add(i => i % 17 == 0, "Buzz");
+            _lienInputResult.Add(i => i % 19 == 0, "Fuzz");
+            _lienInputResult.Add(i => i % 23 == 0, "Bizz");
 
-            return FizzBuzzGameGenerator(input, _lienInputResult);
+            return SuperFizzBuzzGenerator(input, _lienInputResult);
         }
 
         public string SuperFizzBuzz(int input)
         {
-            var result = new StringBuilder();
-            if (input % 3 != 0) result.Append("Fizz");
-            if (input != 0 && input % 2 != 0) result.Append("Buzz");
-            if (Math.Sqrt(input) % 1 == 0) result.Append("Fuzz");
+            
+            _lienInputResult.Add(i => i % 3 != 0, "Fizz");
+            _lienInputResult.Add(i => (i !=0) && (i % 2 != 0), "Buzz");
+            _lienInputResult.Add(i => Math.Sqrt(input)%1 == 0 , "Fuzz");
 
-            return result.Length > 0 ? result.ToString() : input.ToString();
+            var result = SuperFizzBuzzGenerator(input, _lienInputResult);
+            return result;
+
+
         }
 
-        private string FizzBuzzGameGenerator(int input, IDictionary<int, string> lienInputResult)
+        private string SuperFizzBuzzGenerator(int input, IDictionary<Func<int, bool>, string> lienInputResult)
         {
             var result = new StringBuilder();
             foreach (var paire in lienInputResult)
             {
-                if (input % paire.Key == 0) result.Append(paire.Value);
+                if (paire.Key.Invoke(input)) result.Append(paire.Value);
             }
 
             return result.Length > 0 ? result.ToString() : input.ToString();
         }
+
+
+
 
     }
 }
